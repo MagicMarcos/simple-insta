78a3182a7c63422f152a724bb7814229d2c435d5
Marcos Cardoso <76229820+MagicMarcos@users.noreply.github.com>
2021-11-05 14:50:02 -0400

    mvp

---
+++ config/database.js
@@ -0,0 +1,5 @@
+// config/database.js
+module.exports = {
+	url: 'mongodb+srv://savageAuth:w5bzW6ZlKTj4KAvp@cluster0.qbdrb.mongodb.net/gardnerGram?retryWrites=true&w=majority',
+	dbName: 'gardnerGram',
+};
